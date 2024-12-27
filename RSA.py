9c92cf2154a3a41b3bf39ad6723f2b0554429213
Ahmedkhaledd1 <ahmedkhaledno1@gmail.com>
2024-12-28 01:36:42 +0200

    in progress

--- RSA.py
+++ RSA.py
@@ -1,14 +1,6 @@
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
 
-# Generate RSA key pair
-def generate_keypair():
-    key = RSA.generate(2048)
-    private_key = key.export_key()
-    public_key = key.publickey().export_key()
-    return private_key, public_key
-
-
 # Encrypt a message using the public key
 def encrypt(public_key, plaintext):
     rsa_key = RSA.import_key(public_key)
@@ -24,20 +16,3 @@ def decrypt(private_key, ciphertext):
     plaintext = cipher.decrypt(ciphertext)
     return plaintext
 
-
-# Example usage
-if __name__ == "__main__":
-    print("Generating RSA key pair...")
-    private_key, public_key = generate_keypair()
-    print("Public Key:", public_key.decode('utf-8'))
-    print("Private Key:", private_key.decode('utf-8'))
-
-    message = "Secure Communication Suite"
-    print("\nOriginal Message:", message)
-
-
-    encrypted_message = encrypt(public_key, message)
-    print("\nEncrypted Message:", encrypted_message)
-
-    decrypted_message = decrypt(private_key, encrypted_message)
-    print("\nDecrypted Message:", decrypted_message)
